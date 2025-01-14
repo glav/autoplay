2cede8c874ab0f3c2c869665ab66c43907763654
Paul Glavich <paulglavich@microsoft.com>
2025-01-14 06:40:48 +0000

    Added eval agent, fixed error

--- file_reader_agent_using_SelectorGroupChat/agent_init.py
+++ file_reader_agent_using_SelectorGroupChat/agent_init.py
@@ -3,7 +3,6 @@ from local_dir_agent_tool import get_local_disk_files
 from github_agent_tool import get_repository_files
 from autogen_ext.models.openai import OpenAIChatCompletionClient, AzureOpenAIChatCompletionClient
 from autogen_agentchat.agents import AssistantAgent
-from router_agent import RouterAgent
 import logging
 from autogen_core import TRACE_LOGGER_NAME
 import config
@@ -36,9 +35,9 @@ ghAgent = AssistantAgent(
   model_client=chatCompletionClient,
   tools=[get_repository_files],
   system_message="""You are an agent that uses tools to solve tasks related to the files within Github repositories.
-                  You do not answer questions to anything apart from files within github repositories.
-                  If you have provided a sufficient answer to a question about github repository files, you can respond with TERMINATE
-                  to end the conversation.""") # Important: If you do not provide this instruction to terminate, the agents will continue trying to converse and answer questions.
+                  You do not answer questions to anything apart from files within github repositories.""")
+                  # If you have provided a sufficient answer to a question about github repository files, you can respond with TERMINATE
+                  # to end the conversation.""") # Important: If you do not provide this instruction to terminate, the agents will continue trying to converse and answer questions.
 
 
 ldAgent = AssistantAgent(
@@ -47,16 +46,24 @@ ldAgent = AssistantAgent(
   model_client=chatCompletionClient,
   tools=[get_local_disk_files],
   system_message="""You are an agent that uses tools to solve tasks related to files on local disks.
-              You do not solve tasks or answer questions for anything apart from files on a local disk.
-              If you have provided a sufficient answer to a question about local disk files, you can respond with TERMINATE
-              to end the conversation.""") # Important: If you do not provide this instruction to terminate, the agents will continue trying to converse and answer questions.
+              You do not solve tasks or answer questions for anything apart from files on a local disk.""")
+
+evalAgent = AssistantAgent(
+  name="EvaluationAgent",
+  description="This agent evaluates the responses from other agents to determine if the answer supplied is sufficient to answer the  query",
+  model_client=chatCompletionClient,
+  #tools=[get_local_disk_files],
+  system_message="""You are an agent that ensures that answers from other agents are sufficient to answer the query.
+              You do not solve tasks or answer questions about the users query.
+              You must ensure that the answer from another agent is correct and fully answers the query.
+              If you believe the answer from an agent is correct and answers the query, you can respond with TERMINATE""")
 
 text_mention_termination = TextMentionTermination("TERMINATE")
 max_messages_termination = MaxMessageTermination(max_messages=25)
 termination = text_mention_termination | max_messages_termination
 
 team = SelectorGroupChat(
-  [ghAgent, ldAgent],
+  [ghAgent, ldAgent, evalAgent],
   model_client=chatCompletionClient,
   termination_condition=termination,
 )
